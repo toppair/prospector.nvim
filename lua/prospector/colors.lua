f0abb33c79b53d60d0a1831d5ad7b60cdeade2e9
toppair <jbartapp@gmail.com>
2021-11-04 19:01:37 +0100

    Reduce palette, change some hls

--- lua/prospector/colors.lua
+++ lua/prospector/colors.lua
@@ -1,45 +1,18 @@
 return {
   dark = {
     bg = '#1a1a1a',
-    bg_lighten5 = '#262626',
-    s2 = '#333333',
-    s3 = '#4d4d4d',
-    s4 = '#666666',
-    s6 = '#999999',
-    s8 = '#cccccc',
     fg = '#e6e6e6',
-
-    d25 = '#33434d',
-    d35 = '#475d6b',
-    d45 = '#5c798a',
-    d55 = '#7592a3',
-    d65 = '#94abb8',
-
     red = '#d19494',
     green = '#a3d194',
     blue = '#94b2d1',
+    darkblue = '#7592a3',
     yellow = '#d1cc94',
-    darkyellow = '#c9c482',
     sky = '#94d1c7',
     gray = '#778377',
-
-    error = '#ec7979',
   },
   light = {
     bg = '#f2f7ba',
-    bg_darken10 = '#eaf28c',
-    bg_darken20 = '#e1ed5e',
     fg = '#000000',
-    s2 = '#333333',
-    s3 = '#4d4d4d',
-    s4 = '#666666',
-    s6 = '#999999',
-    s8 = '#cccccc',
-
-    b65 = '#c1c884',
-    b70 = '#cad095',
-    b75 = '#d3d8a6',
-
     red = '#660000',
     green = '#006609',
     blue = '#003366',
@@ -47,8 +20,7 @@ return {
     sky = '#006655',
     purple = '#660044',
     gold = '#4d4100',
+    olive = '#c1c884',
     gray = '#333333',
-
-    error = '#991900',
   }
 }
